b185dafaef5ceabe79a4b79fc149547621442e66
Albert Mitto <chrismitto9@gmail.com>
2023-12-03 10:11:24 +0300

    server-side implementation using ExpressJS

---
+++ server.js
@@ -0,0 +1,66 @@
+// server.js
+//
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const mongoose = require('mongoose');
+// const http = require('http');
+// const socketIo = require('socket.io');
+//
+// const app = express();
+// const port = 3000;
+//
+// app.use(bodyParser.json());
+//
+// mongoose.connect('mongodb://localhost/branch-messaging-app', {
+//   useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     });
+//
+//     const messageSchema = new mongoose.Schema({
+//       sender: String,
+//         content: String,
+//           timestamp: { type: Date, default: Date.now },
+//             isRead: { type: Boolean, default: false },
+//             });
+//
+//             const Message = mongoose.model('Message', messageSchema);
+//
+//             const server = http.createServer(app);
+//             const io = socketIo(server);
+//
+//             io.on('connection', (socket) => {
+//               console.log('Client connected');
+//               });
+//
+//               // Simulate the presence of 50+ messages in the database
+//               const seedMessages = async () => {
+//                 for (let i = 1; i <= 50; i++) {
+//                     const newMessage = new Message({
+//                           sender: `Customer ${i}`,
+//                                 content: `This is message number ${i}`,
+//                                     });
+//                                         await newMessage.save();
+//                                           }
+//                                           };
+//
+//                                           seedMessages();
+//
+//                                           app.get('/messages', async (req, res) => {
+//                                             const messages = await Message.find();
+//                                               res.json(messages);
+//                                               });
+//
+//                                               app.post('/messages', async (req, res) => {
+//                                                 const { sender, content } = req.body;
+//                                                   const newMessage = new Message({ sender, content });
+//                                                     await newMessage.save();
+//
+//                                                       io.emit('newMessage', newMessage);
+//
+//                                                         res.status(201).json(newMessage);
+//                                                         });
+//
+//                                                         server.listen(port, () => {
+//                                                           console.log(`Server is running on http://localhost:${port}`);
+//                                                           });
+//
