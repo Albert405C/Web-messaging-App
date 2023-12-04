f6ba5dc75f5574b9daaa3fa4a5f1080671996ec6
Albert Mitto <chrismitto9@gmail.com>
2023-12-04 23:32:59 +0300

    updates

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
 });
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost/messaging_app', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost27017/messaging_app', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
 
