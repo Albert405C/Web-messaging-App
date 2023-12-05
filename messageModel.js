5741b6b30388dbfff54cc54b1f6463639be825e6
Albert Mitto <chrismitto9@gmail.com>
2023-12-05 08:40:42 +0300

    updates

--- messageModel.js
+++ messageModel.js
@@ -15,32 +15,47 @@ const Message = mongoose.model('Message', messageSchema, 'messages');
 
 // Define a function that takes 'socket' and 'io' as parameters
 const initializeSocketListener = (socket, io) => {
-  socket.on('assignMessage', async (data, callback) => {
-    const { messageId, agentId } = data;
-
-    try {
-      const message = await Message.findById(messageId);
-
-      if (!message) {
-        return callback({ error: 'Message not found' });
+    socket.on('assignMessage', async (data, callback) => {
+      const { messageId, agentId } = data;
+  
+      try {
+        // Check if the message exists
+        const message = await Message.findById(messageId);
+  
+        if (!message) {
+          return callback({ error: 'Message not found' });
+        }
+  
+        // Check if the agent (user) exists
+        const isAgentExists = await User.exists({ userID: agentId });
+  
+        if (!isAgentExists) {
+          return callback({ error: 'Agent not found' });
+        }
+  
+        if (message.status === 'unassigned') {
+          // Use $set to update specific fields without affecting others
+          await message.updateOne({ $set: { status: 'assigned', agentId: agentId } });
+  
+          io.emit('messageAssigned', { messageId, agentId });
+  
+          callback({ success: true });
+        } else {
+          // Message is already assigned or completed
+          callback({ error: 'Message already assigned or completed' });
+        }
+      } catch (error) {
+        console.error('Error assigning message:', error);
+  
+        if (error.code === 11000) {
+          // Handle duplicate key error (unique constraint violation) for 'userID'
+          return callback({ error: 'Agent with the same userID already exists' });
+        }
+  
+        callback({ error: 'Internal server error', details: error.message });
       }
-
-      if (message.status === 'unassigned') {
-        // Use $set to update specific fields without affecting others
-        await message.updateOne({ $set: { status: 'assigned', agentId: agentId } });
-
-        io.emit('messageAssigned', { messageId, agentId });
-
-        callback({ success: true });
-      } else {
-        // Message is already assigned or completed
-        callback({ error: 'Message already assigned or completed' });
-      }
-    } catch (error) {
-      console.error('Error assigning message:', error);
-      callback({ error: 'Internal server error', details: error.message });
-    }
-  });
-};
-
-module.exports = { Message, User, initializeSocketListener };
+    });
+  };
+  
+  module.exports = { Message, User, initializeSocketListener };
+  
