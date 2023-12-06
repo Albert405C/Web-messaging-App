3962612495447d7cb4989f7d2a409b52938b5209
Albert Mitto <chrismitto9@gmail.com>
2023-12-06 13:38:25 +0300

    final update

--- messageModel.js
+++ messageModel.js
@@ -1,15 +1,13 @@
-// messageModel.js
-
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 // Define the Message schema
 const messageSchema = new Schema({
-  text: { type: String, required: true },
-  timestamp: { type: Date, default: Date.now },
-  status: { type: String, default: 'unassigned' },
-});
+ text: { type: String, required: true },
+ timestamp: { type: Date, default: Date.now },
+ status: { type: String, default: 'unassigned' },
+}, { collection: 'users' }); // Specify the collection name here
 
-const Message = mongoose.model('Message', messageSchema, 'users');  // Use the correct collection name
+const Message = mongoose.model('Message', messageSchema); // No need to pass the collection name here
 
 module.exports = { Message };
