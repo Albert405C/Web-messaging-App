5649bd8e23c9967bf5a774e585bc0ed8ee244fa9
Albert Mitto <chrismitto9@gmail.com>
2023-12-06 17:03:42 +0300

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import io from 'socket.io-client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const socket = io('http://localhost:3001', {
+const socket = io('http://localhost:3000', {
  withCredentials: true,
  extraHeaders: {
     "my-custom-header": "some-value"
