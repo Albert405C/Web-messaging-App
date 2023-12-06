fa214bb9ad11723304ae7c44574227074e09dfff
Albert Mitto <chrismitto9@gmail.com>
2023-12-06 14:59:33 +0300

    add

--- client/src/App.js
+++ client/src/App.js
@@ -1,86 +1,104 @@
-// src/App.js
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import io from 'socket.io-client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const socket = io('http://localhost:3000');
+const socket = io('http://localhost:3000', {
+ withCredentials: true,
+ extraHeaders: {
+    "my-custom-header": "some-value"
+ }
+});
 
 function App() {
-  const [messages, setMessages] = useState([]);
-  const [newMessage, setNewMessage] = useState({ sender: '', content: '' });
+ const [messages, setMessages] = useState([]);
+ const [newMessage, setNewMessage] = useState({ userId: '', messageBody: '' });
+ const [loading, setLoading] = useState(true);
+ const [error, setError] = useState(null);
 
-  useEffect(() => {
+ useEffect(() => {
     // Fetch initial messages from the server
     axios.get('http://localhost:3000/messages')
-      .then(response => setMessages(response.data))
-      .catch(error => console.error(error));
+      .then(response => {
+        setMessages(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error(error);
+        setError('Error fetching messages');
+        setLoading(false);
+      });
 
-    // Listen for 'newMessage' events from the server
-    socket.on('newMessage', (newMessage) => {
-      setMessages(prevMessages => [...prevMessages, newMessage]);
+    // Listen for 'messageAdded' events from the server
+    socket.on('messageAdded', (newMessage) => {
+      setMessages(prevMessages => [newMessage, ...prevMessages]);
+    });
+
+    // Listen for 'seededMessages' events from the server
+    socket.on('seededMessages', (seededMessages) => {
+      console.log('Received seededMessages:', seededMessages);
+      setMessages(seededMessages);
     });
 
     return () => {
       // Disconnect the socket when the component unmounts
       socket.disconnect();
     };
-  }, []);
+ }, []);
 
-  const handleInputChange = (e) => {
+ const handleInputChange = (e) => {
     const { name, value } = e.target;
     setNewMessage(prevMessage => ({ ...prevMessage, [name]: value }));
-  };
+ };
 
-  const handleSubmit = (e) => {
+ const handleSubmit = (e) => {
     e.preventDefault();
 
-    // Simulate sending a message to the server
-    axios.post('http://localhost:3000/messages', newMessage)
-      .then(response => {
-        // Clear the form and let Socket.IO handle real-time updates
-        setNewMessage({ sender: '', content: '' });
-      })
-      .catch(error => console.error(error));
-  };
+    // Emit 'newMessage' event to the server
+    socket.emit('newMessage', { userId: newMessage.userId, messageBody: newMessage.messageBody }, (response) => {
+      // Handle the acknowledgment from the server (if needed)
+      console.log(response);
+    });
 
-  return (
-    <div>
-      <h1>Branch Messaging App</h1>
-      <ul>
-        {messages.map(message => (
-          <li key={message._id}>
-            <strong>{message.sender}:</strong> {message.content}
-          </li>
-        ))}
-      </ul>
+    // Clear the form
+    setNewMessage({ userId: '', messageBody: '' });
+ };
 
-      {/* Form for sending new messages */}
+ return (
+    <div className="container mt-4">
+      {/* ... (existing code) */}
       <form onSubmit={handleSubmit}>
-        <label>
-          Sender:
-          <input
-            type="text"
-            name="sender"
-            value={newMessage.sender}
-            onChange={handleInputChange}
-          />
-        </label>
-        <br />
-        <label>
-          Content:
-          <input
-            type="text"
-            name="content"
-            value={newMessage.content}
-            onChange={handleInputChange}
-          />
-        </label>
-        <br />
-        <button type="submit">Send Message</button>
+        <div className="mb-3">
+          <label htmlFor="userId" className="form-label">
+            User ID:
+            <input
+              type="text"
+              className="form-control"
+              id="userId"
+              name="userId"
+              value={newMessage.userId}
+              onChange={handleInputChange}
+            />
+          </label>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="messageBody" className="form-label">
+            Message Body:
+            <input
+              type="text"
+              className="form-control"
+              id="messageBody"
+              name="messageBody"
+              value={newMessage.messageBody}
+              onChange={handleInputChange}
+            />
+          </label>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Send Message
+        </button>
       </form>
     </div>
-  );
+ );
 }
-
 export default App;
--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const csvParser = require('csv-parser');
 const cors = require('cors');
 const messageRouter = require('./messageRouter.js');
 const PORT = process.env.PORT || 3000;
-port = 3000;
 const app = express();
 const server = http.createServer(app);
 const Message = require('./messageModel.js');
@@ -35,16 +34,37 @@ mongoose.connect('mongodb://localhost:27017/messaging', { useNewUrlParser: true,
 
 // CSV Parsing and Saving to MongoDB
 const seedMessages = async () => {
-  for (let i = 1; i <= 50; i++) {
-    const newMessage = new Message({
-      sender: `Customer ${i}`,
-      content: `This is message number ${i}`,
-    });
-    await newMessage.save();
-  }
-};
+  const data = [];
 
-seedMessages();
+  return new Promise((resolve, reject) => {
+    fs.createReadStream("C:\\Users\\ADMIN\\OneDrive\\Desktop\\messages.csv")
+      .pipe(parse({ delimiter: ",", from_line: 2 }))
+      .on("data", async function (row) {
+        try {
+          // Properly define userId within the scope of the callback function
+          const userId = row[0].toString();
+
+          // Rest of your code...
+        } catch (error) {
+          console.error("Error processing CSV row:", error);
+          reject(error);
+        }
+      })
+      .on("error", function (error) {
+        console.log(error.message);
+        reject(error);
+      })
+      .on("end", async function () {
+        try {
+          // Rest of your code...
+          resolve(data);
+        } catch (error) {
+          console.error("Error saving CSV data to MongoDB:", error);
+          reject(error);
+        }
+      });
+  });
+};
 
 // Socket.io connection
 // ... (existing code)
@@ -69,16 +89,15 @@ io.on('connection', (socket) => {
 });
 
 
-app.post('/messages', async (req, res) => {
-  const { sender, content } = req.body;
-  const newMessage = new Message({ sender, content });
-  await newMessage.save();
-
-  io.emit('newMessage', newMessage);
-
-  res.status(201).json(newMessage);
-});
-
+app.get('/messages', async (req, res) => {
+  try {
+     const messages = await Message.find(); // Use await to properly handle the async call
+     res.json(messages);
+  } catch (error) {
+     console.error(error);
+     res.status(500).send('Failed to fetch messages');
+  }
+ });
 
 app.use('/', messageRouter);
 server.listen(PORT, () => {
